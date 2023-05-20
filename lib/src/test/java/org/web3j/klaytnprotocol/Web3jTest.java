894e5a1e27d632d03f6d6c923050f6406feb1e1c
kjeom <jack.jin@klaytn.foundation>
2023-05-20 13:57:38 +0900

    integrate klaytnExtension with OpenSDK

--- lib/src/test/java/org/web3j/klaytnprotocol/Web3jTest.java
+++ lib/src/test/java/org/web3j/klaytnprotocol/Web3jTest.java
@@ -4,19 +4,23 @@
 package org.web3j.klaytnprotocol;
 
 import org.junit.Test;
+import org.web3j.protocol.core.methods.response.EthChainId;
 import org.web3j.protocol.core.methods.response.NetVersion;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.Web3j;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Map;
 
 import caver.sdk.apis.KlayApi;
 import caver.sdk.models.GetRewardsResponse;
+import opensdk.sdk.models.GovernanceChainConfigResponse;
 
 public class Web3jTest {
     @Test public void testSomeLibraryMethod() throws IOException {
-        Web3j klaytnWeb3j = new Web3j(new HttpService("http://127.0.0.1:8551"));
-        NetVersion nv = klaytnWeb3j.netVersion().send();
+        Web3j w3 = Web3j.build(new HttpService("http://127.0.0.1:8551"));
+        NetVersion nv = w3.netVersion().send();
         System.out.println(nv.getResult());
     }
 
@@ -25,4 +29,18 @@ public class Web3jTest {
         GetRewardsResponse nv = klaytnWeb3j.getRewards(new BigDecimal(10)).send();
         System.out.println(nv.getResult());
     } 
+
+    @Test public void testSomeLibraryMethod3() throws IOException {
+        KlaytnWeb3j kw3 = KlaytnWeb3j.build(new HttpService("http://127.0.0.1:8551"));
+        GovernanceChainConfigResponse response =  kw3.governance.chainConfig().send();
+        Object cc = response.getResult();
+        System.out.println(cc);
+    }
+
+    @Test public void testSomeLibraryMethod4() throws IOException {
+        KlaytnWeb3j kw3 = KlaytnWeb3j.build(new HttpService("http://127.0.0.1:8551"));
+        EthChainId chainId = kw3.ethChainId().send();
+        String sChainId = chainId.getResult();
+        System.out.println(sChainId);
+    }
 }
